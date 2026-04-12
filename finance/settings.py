48ec179508b8fde3d1e36a772e36576af7f2fe12
Jorge Carvajal <jorgeart15@gmail.com>
2026-04-12 00:08:30 -0600

    Migrate from SQLite to PostgreSQL
    - Update settings.py to use postgresql backend with env-var config
    - Add psycopg[binary] to requirements.txt
    - Add docker-compose.yml with PostgreSQL 17 service and persistent volume

--- finance/settings.py
+++ finance/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'finance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'projectfinance'),
+        'USER': os.environ.get('POSTGRES_USER', 'projectfinance'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'projectfinance'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
